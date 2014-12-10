a0902c27048c11e7c97b75b35c92768326b67a74
David Alberto Montaño Fetecua <davidmontano@seven4n.com>
2014-12-10 10:34:09 -0500

    excercise 6.5

--- src/test/scala/co/damontic/rng/SimpleRNGFunSuite.scala
+++ src/test/scala/co/damontic/rng/SimpleRNGFunSuite.scala
@@ -5,7 +5,7 @@ import org.scalatest.FunSuite
 class SimpleRNGFunSuite extends FunSuite {
 
   import co.damontic.rng.SimpleRNG._
-  
+
   test("SimpleRNG(42).nextInt = 16159453") {
     val (result, nextRNG) = SimpleRNG(42).nextInt
     assert(result === 16159453)
@@ -28,20 +28,27 @@ class SimpleRNGFunSuite extends FunSuite {
     assert(r2 >= 0)
   }
 
-  test("SimpleRNG.double in [0; 1[ ") {
-    val rng1 = SimpleRNG(Int.MaxValue)
-    val (r1, rng1_2) = SimpleRNG.double(rng1)
-    assert(r1 >= 0 && r1 < 1)
-
-    val rng2 = SimpleRNG(Int.MinValue)
-    val (r2, rng2_2) = SimpleRNG.double(rng2)
-    assert(r2 >= 0 && r2 < 1)
-  }
+  //  test("SimpleRNG.double in [0; 1[ ") {
+  //    val rng1 = SimpleRNG(Int.MaxValue)
+  //    val (r1, rng1_2) = SimpleRNG.double(rng1)
+  //    assert(r1 >= 0 && r1 < 1)
+  //
+  //    val rng2 = SimpleRNG(Int.MinValue)
+  //    val (r2, rng2_2) = SimpleRNG.double(rng2)
+  //    assert(r2 >= 0 && r2 < 1)
+  //  }
 
   test("SimpleRNG.ints(10) size of list must be 10 ") {
     val rng = SimpleRNG(Int.MaxValue)
     val (lista, rng1) = SimpleRNG.ints(10)(rng)
     assert(lista.size == 10)
+  }
+
+  test("SimpleRNG.double in [0; 1[ ") {
+    val rng1 = SimpleRNG(Int.MaxValue)
+    val randomMonad : Rand[Double] = SimpleRNG.double2(rng1)
+    val (randomDouble, rng2) = randomMonad(rng1)
+    assert( randomDouble >= 0 && randomDouble < 1)
   }
 
 }
