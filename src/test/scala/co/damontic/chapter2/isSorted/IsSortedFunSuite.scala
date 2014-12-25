9a96faf230193022a98c42ada7787e9dc939d1a4
David Alberto Montaño Fetecua <damontic@gmail.com>
2014-12-25 06:52:20 -0500

    more tests for is ordered with String

--- src/test/scala/co/damontic/chapter2/isSorted/IsSortedFunSuite.scala
+++ src/test/scala/co/damontic/chapter2/isSorted/IsSortedFunSuite.scala
@@ -3,26 +3,40 @@ import co.damontic.chapter2.isSorted.IsSorted
 
 class IsSortedFunSuite extends FunSuite {
 
-  test("given (1,2,3) and function that orders numbers return true") {
-    val arg = Array(1,2,3)
-    val orderedFunction : (Int, Int) => Boolean = {
-      (a: Int, b: Int) => {
-        if(a < b) true
+  val areIntsOrderedFunction: (Int, Int) => Boolean = {
+    (a: Int, b: Int) =>
+      {
+        if (a < b) true
         else false
       }
-    }
-    assert(IsSorted.isSorted(arg, orderedFunction))
+  }
+
+  val areStringsOrderedFunction: (String, String) => Boolean = {
+    (a: String, b: String) =>
+      {
+        if (a < b) true
+        else false
+      }
+  }
+
+  test("given (1,2,3) and function that orders numbers return true") {
+    val arg = Array(1, 2, 3)
+    assert(IsSorted.isSorted(arg, areIntsOrderedFunction))
   }
 
   test("given (2,3,1) and function that orders numbers return false") {
-    val arg = Array(2,3,1)
-    val orderedFunction : (Int, Int) => Boolean = {
-      (a: Int, b: Int) => {
-        if(a < b) true
-        else false
-      }
-    }
-    assert(!IsSorted.isSorted(arg, orderedFunction))
+    val arg = Array(2, 3, 1)
+    assert(!IsSorted.isSorted(arg, areIntsOrderedFunction))
+  }
+
+  test("given ('angela','david','federico') and function that orders Strings return true") {
+    val arg = Array("angela", "david", "federico")
+    assert(IsSorted.isSorted(arg, areStringsOrderedFunction))
+  }
+
+  test("given ('david','angela', 'federico') and function that orders Strings return true") {
+    val arg = Array( "david", "angela","federico")
+    assert(!IsSorted.isSorted(arg, areStringsOrderedFunction))
   }
 
 }
